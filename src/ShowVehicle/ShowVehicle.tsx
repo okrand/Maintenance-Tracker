bf59a6e7ccdcefc8eaa9416277515706fd2a50b0
okrand <orkun.krand@gmail.com>
2019-05-02 20:45:27 -0400

    get react-table to render

--- src/ShowVehicle/ShowVehicle.tsx
+++ src/ShowVehicle/ShowVehicle.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Vehicle, getLog } from "../helpers";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { Vehicle, getLog, getColumns } from "../helpers";
 
 interface Props {
     vehicle: Vehicle
@@ -13,13 +15,14 @@ export default class ShowVehicle extends Component<Props, {}> {
     render() {
         const displayVehicleName = `${this.props.vehicle.year} ${this.props.vehicle.make} ${this.props.vehicle.model}`;
         const log = getLog(this.props.vehicle.id);
-        const displayLogs = log && log.map((l) => {
-            return <p key={l.id}>{l.work}, {l.mileage}</p>
-        });
+        const nextOilChange = "";
+        const columns = getColumns();
         return (<div>
-            <div className="vehicle-name"> {displayVehicleName} </div>
-            <div className="next-oil"></div>
-            <div className="logs">{displayLogs}</div>
+            <div className="vehicle-header"> 
+                <div className="vehicle-name">{displayVehicleName} </div>
+                <div className="vehicle-next-oil">{nextOilChange}</div>
+            </div>
+            <ReactTable className="vehicle-table" data={log} columns={columns} defaultPageSize = {10}/>
             </div>);
     }
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -40,7 +40,8 @@ interface Log {
     work: string,
     mileage: number,
     category: string,
-    details: string
+    details: string,
+    date: number
 }
 
 export function getLog(vehicleId: number): Log[] | undefined {
@@ -51,10 +52,36 @@ export function getLog(vehicleId: number): Log[] | undefined {
                 vehicleId: 0,
                 work: "Oil Change w/ Filter",
                 mileage: 28875,
+                date: new Date(2019, 5, 1).valueOf(),
+                category: "Oil Change",
+                details: "Mobil1 + HiFlo filter"
+            },
+            {
+                id: 1,
+                vehicleId: 0,
+                work: "Oil Change w/ Filter",
+                mileage: 28875,
+                date: new Date(2019, 4, 29).valueOf(),
                 category: "Oil Change",
                 details: "Mobil1 + HiFlo filter"
             }
         ];
     }
     return undefined;
+}
+
+export function getColumns() {
+    return [{
+        Header: "Work",
+        accessor: "work"
+    },{
+        Header: "Date",
+        accessor: "date"
+    },{
+        Header: "Mileage",
+        accessor: "mileage"
+    },{
+        Header: "Details",
+        accessor: "details"
+    }];
 }
